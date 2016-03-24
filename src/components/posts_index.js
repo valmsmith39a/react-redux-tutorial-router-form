0e13654d17784b11faa27354b61133b92e7fe340
valmsmith39a <valmsmith39a@gmail.com>
2016-03-24 13:01:26 -0700

    Implements button to add post that links to create form component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   // A life-cycle method Will call when component will mount 1st time
@@ -10,7 +11,14 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
     );
   }
 }
