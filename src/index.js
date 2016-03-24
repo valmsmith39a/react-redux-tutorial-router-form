78978239eae6e9c85f12e6217c13926635982075
valmsmith39a <valmsmith39a@gmail.com>
2016-03-24 11:15:28 -0700

    Implements action creator

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
+import promise from 'redux-promise';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
