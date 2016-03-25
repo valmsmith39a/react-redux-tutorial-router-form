2d948519b90c39018d27bb0495c732cd7ad8a671
valmsmith39a <valmsmith39a@gmail.com>
2016-03-24 17:59:56 -0700

    Completes delete functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=asfjaskjfakldfsfja';
@@ -17,7 +18,6 @@ export function fetchPosts() {
 
 export function createPost(props) {
   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
-
   return {
     type: CREATE_POST,
     payload: request
@@ -26,9 +26,16 @@ export function createPost(props) {
 
 export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
-
   return {
     type: FETCH_POST,
     payload:request
   }
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: DELETE_POST,
+    payload: request
+  };
 }
