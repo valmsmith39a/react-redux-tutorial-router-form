68ee7490f533e123d28b4d6d7e5bf12a9082825a
valmsmith39a <valmsmith39a@gmail.com>
2016-03-24 17:14:45 -0700

    Implements links to list of items

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,34 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  // props is properties from our form
+  onSubmit(props) {
+    this.props.createPost(props)
+    .then(() => {
+      // blog post has been created, navigate user to the index
+      // navigate by calling this.context.router.push with the new
+      // path to navigate to
+      this.context.router.push('/');
+    });
+  }
   render() {
     const { fields: {title, categories, content}, handleSubmit } = this.props;
-
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
-        <h3> Create a new post</h3>
-        <div className="form-group">
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <h3> Create a new Post</h3>
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label htmlFor="">Title</label>
-          <input type="text" className="form-control" {...title}/>
+          <input type="text" className="form-control" {...title} />
           <div className="text-help">
             { title.touched ? title.error : ''}
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label htmlFor="">Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            { categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label htmlFor="">Content</label>
-          <input type="text" className="form-control" {...content}/>
+          <input type="text" className="form-control" {...content} />
+          <div className="text-help">
+            { content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -38,6 +59,12 @@ function validate(values) {
   const errors = {};
   if(!values.title) {
     errors.title = 'Enter a username';
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+  if(!values.content) {
+    errors.content = 'Enter content';
   }
   return errors;
 }
