5fcea0b7e4dd259ce70ab98702da3094c001b34a
valmsmith39a <valmsmith39a@gmail.com>
2016-03-24 17:32:04 -0700

    Shows posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=asfjaskjfakldfsfja';
@@ -22,3 +23,12 @@ export function createPost(props) {
     payload: request
   };
 };
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload:request
+  }
+}
